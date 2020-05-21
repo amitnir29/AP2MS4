bb3399c0a9785d375c65a0015d580e6097e54211
Amit-Milo <56229281+Amit-Milo@users.noreply.github.com>
2020-05-21 18:43:41 +0300

    Update IFlightsModel.cs

--- FlightControl/FlightControlWeb/Model/IFlightsModel.cs
+++ FlightControl/FlightControlWeb/Model/IFlightsModel.cs
@@ -9,9 +9,13 @@ namespace FlightControlWeb.Model
 {
     public interface IFlightsModel
     {
+        public FlightPlan GetFlightPlan(string id);
+
         public IList<Flight.Flight> GetAllFlights();
 
         public IList<Flight.Flight> GetAllFlightsSync();
+
+        public void AddFlightPlan(FlightPlan plan);
 
         public void DeleteFlight(string id);
     }
