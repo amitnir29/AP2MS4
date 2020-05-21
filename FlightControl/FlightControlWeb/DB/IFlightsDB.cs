8f05c6f906a5cc883fef0b478174b34e6638e29e
Amit-Milo <56229281+Amit-Milo@users.noreply.github.com>
2020-05-21 18:30:23 +0300

    Initial Commit

---
+++ FlightControl/FlightControlWeb/DB/IFlightsDB.cs
@@ -0,0 +1,22 @@
+﻿using FlightControlWeb.Flight;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb.DB
+{
+    interface IFlightsDB
+    {
+
+        public Task<ActionResult<FlightPlan>> PostFlightPlan(FlightPlan flightPlan);
+
+        public Task<ActionResult<FlightPlan>> GetFlightPlan(string id);
+
+        public void DeleteFlightPlan(string id);
+
+        public Task<ActionResult<IEnumerable<FlightPlan>>> GetIterator();
+
+    }
+}
---
+++ FlightControl/FlightControlWeb/HTTPServer/FlightsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using FlightControlWeb.DB;
+
+namespace FlightControlWeb.HTTPServer
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightsController : ControllerBase
+    {
+        private static IFlightsDB flightsDB;
+
+        // GET: api/Flights
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        // GET: api/Flights/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            flightsDB.DeleteFlightPlan(id);
+        }
+    }
+}
