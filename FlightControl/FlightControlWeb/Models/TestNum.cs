3a43755bd47c9c72cf4ce3dad0e6e4b032600b04
YoavCarmel <carmel.yoav@gmail.com>
2020-05-24 15:45:09 +0300

    tried creating a web app with all 4 http requests, did not work that well

---
+++ FlightControl/FlightControlWeb/Models/TestNum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb.Models
+{
+    public class TestNum
+    {
+        /*
+        public TestNum(int id, int value)
+        {
+            Id = id;
+            Value = value;
+        }
+        */
+
+        public int Id
+        {
+            get; set;
+        }
+        public int Value
+        {
+            get; set;
+        }
+    }
+}
