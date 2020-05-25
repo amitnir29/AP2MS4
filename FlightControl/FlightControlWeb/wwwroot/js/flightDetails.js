c498c65c9e40be8feac786ae384195b4cdbfeafe
RonSherman <wwwronwww1@gmail.com>
2020-05-25 03:03:14 +0300

    Worked on map

--- FlightControl/FlightControlWeb/wwwroot/js/flightDetails.js
+++ FlightControl/FlightControlWeb/wwwroot/js/flightDetails.js
@@ -4,7 +4,7 @@
     <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     */
 
-const fd = document.getElementById("flightDetails");
+/*const fd = document.getElementById("flightDetails");
 fd.innerHTML += "<h3>flight details</h3>\
     <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p >\
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>";
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>";*/
