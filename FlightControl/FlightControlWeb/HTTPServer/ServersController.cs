898b886c3942ca82fc619e19fda220fb95d36c40
Amit-Milo <56229281+Amit-Milo@users.noreply.github.com>
2020-05-27 17:06:04 +0300

    Start creating ServersController

---
+++ FlightControl/FlightControlWeb/HTTPServer/ServersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightControlWeb.HTTPServer
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServersController : ControllerBase
+    {
+        // GET: api/Servers
+        [HttpGet]
+        public async Task<IList<Server>> Get()
+        {
+            
+        }
+
+        // POST: api/Servers
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
