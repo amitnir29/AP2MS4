64cd9c8beceb4492f64585c2c47f33837f6a95f5
RonSherman <wwwronwww1@gmail.com>
2020-06-09 03:28:20 +0300

    Added Drag&Drop functionallity

--- FlightControl/FlightControlWeb/wwwroot/js/DragAndDrop.js
+++ FlightControl/FlightControlWeb/wwwroot/js/DragAndDrop.js
@@ -1,25 +1,47 @@
 ﻿//get the item of the list, add to the dragover event to become a d&d zone, and drag leave come back
-/*let dropArea = document.getElementById('drop-area')
+let dropArea = document.getElementById('flightsLists');
 
-  dropArea.addEventListener('dragenter', handlerFunction, false)
+  /*dropArea.addEventListener('dragenter', handlerFunction, false)
   dropArea.addEventListener('dragleave', handlerFunction, false)
   dropArea.addEventListener('dragover', handlerFunction, false)
-  dropArea.addEventListener('drop', handlerFunction, false)
+  dropArea.addEventListener('drop', handlerFunction, false)*/
+    ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, preventDefaults, false)
+    });
 
-['dragenter', 'dragover'].forEach(eventName => {
-  dropArea.addEventListener(eventName, highlight, false)
-})
+    function preventDefaults(e) {
+        e.preventDefault()
+        e.stopPropagation()
+    }
+    ;['dragenter', 'dragover'].forEach(eventName => {
+        dropArea.addEventListener(eventName, highlight, false)
+    })
 
-;['dragleave', 'drop'].forEach(eventName => {
-  dropArea.addEventListener(eventName, unhighlight, false)
-})
+        ;['dragleave', 'drop'].forEach(eventName => {
+            dropArea.addEventListener(eventName, unhighlight, false)
+        })
 
-function highlight(e) {
-  dropArea.classList.add('highlight')
-}
+    function highlight(e) {
+        dropArea.classList.add('highlight')
+        console.log("highlight");
+    }
 
-function unhighlight(e) {
-  dropArea.classList.remove('highlight')
-}
+    function unhighlight(e) {
+        dropArea.classList.remove('highlight')
+        console.log("unhighlight");
+    }
 
-  */
+    dropArea.addEventListener('drop', handleDrop, false)
+
+    function handleDrop(e) {
+        let dt = e.dataTransfer
+        let files = dt.files
+
+        handleFiles(files)
+    }
+    function handleFiles(files) {
+        ([...files]).forEach(uploadFile)
+    }
+
+
+ 
